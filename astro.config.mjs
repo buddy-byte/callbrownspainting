a9e89f97f97e70f4501f092980af9ce99ba5096a
Buddy Barnes <buddy@barnescom.com>
2025-10-22 19:38:12 -0500

    Force Node.js 20 in package.json engines for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,12 @@ export default defineConfig({
     webAnalytics: {
       enabled: true
     },
-    functionPerRoute: false
+    functionPerRoute: false,
+    imageService: true,
+    imagesConfig: {
+      sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      domains: []
+    }
   }),
   integrations: [
     sitemap()
